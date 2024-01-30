b9f9117b0d452894c069b36ffe1febb748c4b72f
Artem Pechenkin <ghost.0302@yandex.ru>
2024-01-30 18:45:54 +0300

    Добавьте файлы проекта.

---
+++ CshrpPR1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CshrpPR1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int x2 = x * x;
+            double y = x2 - 7 * x + 10;
+            double y2 = x2 - 8 * x + 12;
+            Console.WriteLine(y / y2);
+        }
+    }
+}
---
+++ Пр2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Пр2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string x = ""; 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x = textBox1.Text;
+            textBox1.Text = textBox2.Text;
+            textBox2.Text = x;
+        }
+    }
+}
